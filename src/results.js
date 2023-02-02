5739404cff177ff9f62364310ef0bd31914061a2
aswinina <116088497+aswinina@users.noreply.github.com>
2023-02-02 13:45:51 +0100

    added the phonetic functions

--- src/results.js
+++ src/results.js
@@ -1,11 +1,20 @@
 import React from "react";
-import Meaning from "./meaning"
+import Meaning from "./meaning";
+import Phonetic from "./phonetic";
 
 export default function Results(props){
     if(props.results){
 return(
         <div className="results">
 <h2>{props.results.word}</h2>
+{props.results.phonetics.map(function(phonetic, index){
+    return(
+        <div key={index}>
+            <Phonetic phonetic={phonetic} />
+        </div>
+    )
+})}
+
 {props.results.meanings.map(function(meaning, index){
     return (
         <div key={index}>
