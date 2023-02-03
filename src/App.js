e05cf083f6151b866ab984a96714018446f6dc29
aswinina <116088497+aswinina@users.noreply.github.com>
2023-02-03 13:52:12 +0100

    improve the styles

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Dictionary from "./dictionary";
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         
       </header>
@@ -12,8 +13,10 @@ export default function App() {
         <Dictionary defaultKeyword="hello"/>
       </main>
       <footer className='App-footer'>
-        coded by Aswini Sureddy
+       This project was coded by <a href="https://aswinisureddy-profile.netlify.app/" >Aswini Sureddy</a> 
+       and is <a href="https://github.com/aswinina/dictionary-project" >open-sourced on GitHub</a> and <a href="https://dictionary-app-project-build.netlify.app/">hosted on Netlify</a> 
       </footer>
+    </div>
     </div>
   );
 }
